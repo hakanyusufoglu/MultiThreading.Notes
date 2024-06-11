89455ae98cb5ca5ad3685b808b0a8d880e7a051d
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-06-11 15:02:08 +0300

    Thread Id örneği gerçekleştirildi.

--- MultiThreading.Notes.Lesson1/Program.cs
+++ MultiThreading.Notes.Lesson1/Program.cs
@@ -1,22 +1,59 @@
-﻿class Program
+﻿#region Worker Thread, Main Thread
+//class Program
+//{
+
+//    //Main Thread
+//    private static void Main(string[] args)
+//    {
+
+//        //Worker Thread
+//        Thread thread = new((O) =>
+//        {
+//            for (int i = 0; i < 99; i++)
+//            {
+//                Console.WriteLine($"Worker Thread {i}");
+//            }
+//        });
+
+//        thread.Start();
+
+//        for (int i = 0; i < 99; i++)
+//        {
+//            Console.WriteLine($"Main Thread {i}");
+//        } 
+//    }
+
+//}
+#endregion
+
+#region Thread Id
+
+class Program
 {
-    //Main Thread
     private static void Main(string[] args)
     {
-        //Worker Thread
-        Thread thread = new((O) =>
-        {
-            for (int i = 0; i < 99; i++)
-            {
-                Console.WriteLine($"Worker Thread {i}");
-            }
-        });
+        //Main thread, Thread Id
+        Console.WriteLine("Main Thread");
+        Console.WriteLine(Environment.CurrentManagedThreadId);
+        Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
 
+        Thread thread = new(() =>
+        {
+            //Worker thread, Thread Id
+            Console.WriteLine("Worker Thread");
+            Console.WriteLine(Environment.CurrentManagedThreadId);
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        });
         thread.Start();
 
-        for (int i = 0; i < 99; i++)
+        Thread thread2 = new(() =>
         {
-            Console.WriteLine($"Main Thread {i}");
-        }
+            //Worker thread, Thread Id
+            Console.WriteLine("Worker Thread 2");
+            Console.WriteLine(Environment.CurrentManagedThreadId);
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        });
+        thread2.Start();
     }
 }
+#endregion
