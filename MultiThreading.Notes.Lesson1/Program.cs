e84a3f3e16c796ded6aefdcd74d63afe1cb5e3e5
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-06-11 14:47:47 +0300

    Worker thread, main thread örneği gerçekleştirildi.

--- MultiThreading.Notes.Lesson1/Program.cs
+++ MultiThreading.Notes.Lesson1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    //Main Thread
+    private static void Main(string[] args)
+    {
+        //Worker Thread
+        Thread thread = new((O) =>
+        {
+            for (int i = 0; i < 99; i++)
+            {
+                Console.WriteLine($"Worker Thread {i}");
+            }
+        });
+
+        thread.Start();
+
+        for (int i = 0; i < 99; i++)
+        {
+            Console.WriteLine($"Main Thread {i}");
+        }
+    }
+}
