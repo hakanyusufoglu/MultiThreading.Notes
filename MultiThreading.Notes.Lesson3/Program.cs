9175bdc31d14b135058c5703361b0a40e6f3acff
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-06-12 10:12:04 +0300

    Monitor.Enter ve Monitor.Exit kullanımından önce locking mekanizmasıyla aynı örneğin gerçekleştirilmesi tamamlandı

--- MultiThreading.Notes.Lesson3/Program.cs
+++ MultiThreading.Notes.Lesson3/Program.cs
@@ -1,43 +1,84 @@
 ﻿#region Spinning
+//internal class Program
+//{
+//    private static void Main(string[] args)
+//    {
+//        //Spinning belli bir koşul sağlandığında döngüden çıkılması gereken durumlarda kullanılır.
+//        //Şartla bir threadin içerisinde döngü oluşturulur ve bu döngüde bir koşul sağlandığında döngüden çıkılır.
+//        //Sürekli olarak işlemciyi meşgul eder ve performans kaybına neden olur. Çünkü while döngüsü sürekli olarak çalışır ve işlemciyi meşgul eder.
+//        bool threadCondition = true;
+
+//        Thread thread1 = new(() =>
+//        {
+//            while (true)
+//            {
+//                if (threadCondition)
+//                {
+//                    for (int i = 0; i < 10; i++)
+//                    {
+//                        Console.WriteLine($"Thread 1 {i}");
+//                    }
+
+//                    threadCondition = false;
+//                    break;
+//                }
+//            }
+//        });
+
+//        Thread thread2 = new(() =>
+//        {
+//            while (true)
+//            {
+//                if (!threadCondition)
+//                {
+//                    for (int i = 10; i > 0; i--)
+//                    {
+//                        Console.WriteLine($"Thread 2 {i}");
+//                    }
+
+//                    break;
+//                }
+//            }
+//        });
+
+//        thread1.Start();
+//        thread2.Start();
+//    }
+//}
+#endregion
+
+#region Monitor.Enter ve Monitor.Exit
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-        //Spinning belli bir koşul sağlandığında döngüden çıkılması gereken durumlarda kullanılır.
-        //Şartla bir threadin içerisinde döngü oluşturulur ve bu döngüde bir koşul sağlandığında döngüden çıkılır.
-        //Sürekli olarak işlemciyi meşgul eder ve performans kaybına neden olur. Çünkü while döngüsü sürekli olarak çalışır ve işlemciyi meşgul eder.
-        bool threadCondition = true;
+        //Ortak kaynak üzerinde işlem yaparken bir threadin diğer threadin işlemine müdahale etmemesi için Monitor.Enter ve Monitor.Exit kullanılır.
+        //Monitor.Enter ve Monitor.Exit kullanıldığında bir thread bir kaynağı işgal ettiğinde diğer thread o kaynağı işgal edemez.
+
+        //Ortak kaynak
+        int i = 0;
+        object locking = new();
 
         Thread thread1 = new(() =>
         {
-            while (true)
+            lock (locking)
             {
-                if (threadCondition)
+                for (i = 0; i < 10; i++)
                 {
-                    for (int i = 0; i < 10; i++)
-                    {
-                        Console.WriteLine($"Thread 1 {i}");
-                    }
-
-                    threadCondition = false;
-                    break;
+                    Console.WriteLine($"Thread 1 {i}");
                 }
             }
         });
 
         Thread thread2 = new(() =>
         {
-            while (true)
+            lock (locking)
             {
-                if (!threadCondition)
+                for (i = 0; i < 10; i++)
                 {
-                    for (int i = 10; i > 0; i--)
-                    {
-                        Console.WriteLine($"Thread 2 {i}");
-                    }
-
-                    break;
-                }
+                    Console.WriteLine($"Thread 2 {i}");
+                } 
             }
         });
 
