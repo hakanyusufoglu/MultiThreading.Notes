0f63d79a0889c893813449f49a2761955064554e
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-06-12 15:59:24 +0300

    SemaphoreSlim ile ilgili örnek gerçekleştirildi

--- MultiThreading.Notes.Lesson4/Program.cs
+++ MultiThreading.Notes.Lesson4/Program.cs
@@ -1,4 +1,67 @@
-﻿#region Semophore 
+﻿#region Semaphore 
+//internal class Program
+//{
+//    private static void Main(string[] args)
+//    {
+//        List<int> numbers = new();
+
+//        //Semaphore ortak kaynağı maximum 2 tane thread kullanabilir. Bir thread işlemi bitirince diğer thread işlemi başlar.
+//        Semaphore semaphore = new(2, 2);
+
+//        Thread thread1 = new(() =>
+//        {
+//            //Semaphore.WaitOne() metodu ile semaphore nesnesinin sayısını azaltıyoruz.
+//            semaphore.WaitOne();
+//            int i = 0;
+
+//            while (i < 10)
+//            {
+//                Console.WriteLine($"Thread 1 {++i}");
+//                numbers.Add(i);
+//                Thread.Sleep(1000);
+//            }
+//            //Semaphore.Release() metodu ile semaphore nesnesinin sayısını arttırıyoruz. Yani bir thread işlemi bittiğinde diğer thread işlemi başlar.
+//            semaphore.Release();
+//        });
+
+//        Thread thread2 = new(() =>
+//        {
+//            semaphore.WaitOne();
+//            int i = 10;
+
+//            while (i < 20)
+//            {
+//                Console.WriteLine($"Thread 2 {++i}");
+//                numbers.Add(i);
+//                Thread.Sleep(1500);
+//            }
+
+//            semaphore.Release();
+//        });
+
+//        Thread thread3 = new(() =>
+//        {
+//            semaphore.WaitOne();
+//            int i = 20;
+
+//            while (i < 30)
+//            {
+//                Console.WriteLine($"Thread 3 {++i}");
+//                numbers.Add(i);
+//                Thread.Sleep(2000);
+//            }
+
+//            semaphore.Release();
+//        });
+
+//        thread1.Start();
+//        thread2.Start();
+//        thread3.Start();
+//    }
+//}
+#endregion
+
+#region SemaphoreSlim
 internal class Program
 {
     private static void Main(string[] args)
@@ -6,12 +69,13 @@ internal class Program
         List<int> numbers = new();
 
         //Semaphore ortak kaynağı maximum 2 tane thread kullanabilir. Bir thread işlemi bitirince diğer thread işlemi başlar.
-        Semaphore semaphore = new(2, 2);
+        //Semaphoredan farkı daha hızlı çalışmasıdır. asenkron operasyonlar için daha uygun bir yapıdır.
+        SemaphoreSlim semaphoreSlim = new(2, 2);
 
         Thread thread1 = new(() =>
         {
-            //Semaphore.WaitOne() metodu ile semaphore nesnesinin sayısını azaltıyoruz.
-            semaphore.WaitOne();
+            //SemaphoreSlim.Wait() metodu ile semaphore nesnesinin sayısını azaltıyoruz.
+            semaphoreSlim.Wait();
             int i = 0;
 
             while (i < 10)
@@ -21,12 +85,12 @@ internal class Program
                 Thread.Sleep(1000);
             }
             //Semaphore.Release() metodu ile semaphore nesnesinin sayısını arttırıyoruz. Yani bir thread işlemi bittiğinde diğer thread işlemi başlar.
-            semaphore.Release();
+            semaphoreSlim.Release();
         });
 
         Thread thread2 = new(() =>
         {
-            semaphore.WaitOne();
+            semaphoreSlim.Wait();
             int i = 10;
 
             while (i < 20)
@@ -36,12 +100,12 @@ internal class Program
                 Thread.Sleep(1500);
             }
 
-            semaphore.Release();
+            semaphoreSlim.Release();
         });
 
         Thread thread3 = new(() =>
         {
-            semaphore.WaitOne();
+            semaphoreSlim.Wait();
             int i = 20;
 
             while (i < 30)
@@ -51,7 +115,7 @@ internal class Program
                 Thread.Sleep(2000);
             }
 
-            semaphore.Release();
+            semaphoreSlim.Release();
         });
 
         thread1.Start();
