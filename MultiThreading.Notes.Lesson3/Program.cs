45f941702dccbd84c37e7de074f4badbef6546ce
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-06-12 10:36:02 +0300

    lockTaken parametresi ile örnek gerçekleştirilid.

--- MultiThreading.Notes.Lesson3/Program.cs
+++ MultiThreading.Notes.Lesson3/Program.cs
@@ -48,17 +48,69 @@
 #endregion
 
 #region Monitor.Enter ve Monitor.Exit
+//internal class Program
+//{
+//    private static void Main(string[] args)
+//    {
+//        //Ortak kaynak üzerinde işlem yaparken bir threadin diğer threadin işlemine müdahale etmemesi için Monitor.Enter ve Monitor.Exit kullanılır.
+//        //Monitor.Enter ve Monitor.Exit kullanıldığında bir thread bir kaynağı işgal ettiğinde diğer thread o kaynağı işgal edemez.
 
+//        //Monitor enter ve exit kullanıldığında bir hata meydana geldiğinde kaynak serbest bırakılmayacaktır. hatanın alındığı yeerde monitor exit kullanılmalıdır. Bu durum için finnaly bloğu kullanılabilir.
+
+//        //Lock mekanizması bu durumu otomatik olarak yapar. Yani kaynağı işgal eden thread işini bitirdiğinde kaynağı serbest bırakır. Monitor enter ve exit kullanıldığında bu işlemi manuel olarak yapmak gerekir.
+
+//        //Ortak kaynak
+//        int i = 0;
+
+//        object locking = new();
+
+//        Thread thread1 = new(() =>
+//        {
+//            try
+//            {
+//                Monitor.Enter(locking);
+
+//                for (i = 0; i < 10; i++)
+//                {
+//                    Console.WriteLine($"Thread 1 {i}");
+//                }
+//            }
+//            finally
+//            {
+//                Monitor.Exit(locking);
+//            }
+
+//        });
+
+//        Thread thread2 = new(() =>
+//        {
+//            try
+//            {
+//                Monitor.Enter(locking);
+
+//                for (i = 0; i < 10; i++)
+//                {
+//                    Console.WriteLine($"Thread 2 {i}");
+//                }
+//            }
+//            finally
+//            {
+//                Monitor.Exit(locking);
+//            }
+//        });
+
+//        thread1.Start();
+//        thread2.Start();
+//    }
+//}
+#endregion
+
+#region LockTaken
 internal class Program
 {
     private static void Main(string[] args)
     {
-        //Ortak kaynak üzerinde işlem yaparken bir threadin diğer threadin işlemine müdahale etmemesi için Monitor.Enter ve Monitor.Exit kullanılır.
-        //Monitor.Enter ve Monitor.Exit kullanıldığında bir thread bir kaynağı işgal ettiğinde diğer thread o kaynağı işgal edemez.
-
-        //Monitor enter ve exit kullanıldığında bir hata meydana geldiğinde kaynak serbest bırakılmayacaktır. hatanın alındığı yeerde monitor exit kullanılmalıdır. Bu durum için finnaly bloğu kullanılabilir.
-
-        //Lock mekanizması bu durumu otomatik olarak yapar. Yani kaynağı işgal eden thread işini bitirdiğinde kaynağı serbest bırakır. Monitor enter ve exit kullanıldığında bu işlemi manuel olarak yapmak gerekir.
+        //lockToken parametresi: lock mekanizmasının işlemi başarılı bir şekilde gerçekleşip gerçekleşmediğini kontrol eder. Çünkü Monitor.Enter ve Monitor.Exit lock işleminin başarılı bir şekilde gerçekleştiğinin garantisini vermemektedir.
 
         //Ortak kaynak
         int i = 0;
@@ -69,12 +121,12 @@ internal class Program
         {
             try
             {
-                Monitor.Enter(locking);
+                bool lockTaken = false;
+                Monitor.Enter(locking, ref lockTaken);
 
-                for (i = 0; i < 10; i++)
-                {
-                    Console.WriteLine($"Thread 1 {i}");
-                }
+                if (lockTaken)
+                    for (i = 0; i < 10; i++)
+                        Console.WriteLine($"Thread 1 {i}");
             }
             finally
             {
@@ -87,12 +139,12 @@ internal class Program
         {
             try
             {
-                Monitor.Enter(locking);
+                bool lockTaken = false;
+                Monitor.Enter(locking, ref lockTaken);
 
-                for (i = 0; i < 10; i++)
-                {
-                    Console.WriteLine($"Thread 2 {i}");
-                }
+                if (lockTaken)
+                    for (i = 0; i < 10; i++)
+                        Console.WriteLine($"Thread 2 {i}");
             }
             finally
             {
