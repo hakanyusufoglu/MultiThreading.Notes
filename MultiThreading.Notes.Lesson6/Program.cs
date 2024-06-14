841bb323113779049aaec1e6c28932da4d0b3ef6
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-06-14 14:57:01 +0300

    MemoryBarrier örneği gerçekleştirildi

--- MultiThreading.Notes.Lesson6/Program.cs
+++ MultiThreading.Notes.Lesson6/Program.cs
@@ -1,48 +1,85 @@
 ﻿#region Interlocked Class
+//internal class Program
+//{
+//    private static void Main(string[] args)
+//    {
+//        //Ortak kaynak
+//        int i = 0;
+
+//        //İ değeri 5 olur
+//        Interlocked.Exchange(ref i, 5);
+
+//        //i değeri 5 ise 10 yap
+//        Interlocked.CompareExchange(ref i, 10, 5);
+
+//        Thread thread1 = new(() =>
+//        {
+//            while (true)
+//            {
+//                //ortak kaynak olduğu için risk = i++;
+
+//                //Senkronizasyon sağlar
+//                Interlocked.Increment(ref i);
+//            }
+//        });
+
+//        Thread thread2 = new(() =>
+//        {
+//            while (true)
+//            {
+//                Console.WriteLine(i++);
+//            }
+//        });
+
+//        Thread thread3 = new(() =>
+//        {
+//            while (true)
+//            {
+//                //risk i--+;
+//                Interlocked.Decrement(ref i);
+//            }
+//        });
+
+//        thread1.Start();
+//        thread2.Start();
+//        thread3.Start();
+//    }
+//}
+#endregion
+#region MemoryBarrier
 internal class Program
 {
     private static void Main(string[] args)
     {
-        //Ortak kaynak
+        //ortak kaynak
         int i = 0;
 
-        //İ değeri 5 olur
-        Interlocked.Exchange(ref i, 5);
-
-        //i değeri 5 ise 10 yap
-        Interlocked.CompareExchange(ref i, 10, 5);
-
-        Thread thread1 = new(() =>
+        Thread writeThread = new(() =>
         {
             while (true)
             {
-                //ortak kaynak olduğu için risk = i++;
-
                 //Senkronizasyon sağlar
                 Interlocked.Increment(ref i);
+
+                //MemoryBarrier ile tüm threadlerin aynı değere ulaşmasını sağlar
+                Thread.MemoryBarrier();
             }
         });
 
-        Thread thread2 = new(() =>
+        Thread readThread = new(() =>
         {
             while (true)
             {
-                Console.WriteLine(i++);
+                //Güncel değeri okur. Ancak MemoryBarrier nerede çağrılıcağından emin olunmalıdır
+                Thread.MemoryBarrier();
+
+                Console.WriteLine(i);
             }
         });
 
-        Thread thread3 = new(() =>
-        {
-            while (true)
-            {
-                //risk i--+;
-                Interlocked.Decrement(ref i);
-            }
-        });
-
-        thread1.Start();
-        thread2.Start();
-        thread3.Start();
+        writeThread.Start();
+        readThread.Start();
     }
 }
+
 #endregion
