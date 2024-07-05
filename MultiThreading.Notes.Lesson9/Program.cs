e2ab76ad7d0d25fae8d9951da545e87c97d68e51
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-07-05 08:51:28 +0300

    ManualResetEventSlim ile ilgili örnek gerçekleştirildi

--- MultiThreading.Notes.Lesson9/Program.cs
+++ MultiThreading.Notes.Lesson9/Program.cs
@@ -1,40 +1,92 @@
 ﻿
 #region AutoResetEvent
+//internal class Program
+//{
+//    private static void Main(string[] args)
+//    {
+//        //Örneğin thread1 işlemini bitirdikten sonra thread2 işlemi başlamasını istiyorsak EventWaitHandle kullanabiliriz.
+//        //AutoReset her zaman otomatik olarak resetlenir bunun faydası ise bir thread işlemi bittiğinde diğer thread işlemi başlar.
+//        //Her bir thread kuyruğa alınır ve sırayla işlem yaparlar.
+//        AutoResetEvent autoResetEvent = new(false);
+
+//        Thread thread1 = new(() =>
+//{
+//    Console.WriteLine("Thread1");
+
+//    //Thread1 işlemi bittiğinde autoResetEvent.Set() metodu ile thread2 işlemine geçiş yapılmasını sağlıyoruz.
+//    //Bekleyen threadlerden sadece bir tanesi başlatılır.
+//    autoResetEvent.Set();
+//});
+
+//Thread thread2 = new(() =>
+//{
+//    //Thread1 işlemi bittikten sonra thread2 işlemi başlar. thread2 şuan beklemesini sağlıyoruz.
+//    autoResetEvent.WaitOne();
+//    Console.WriteLine("Thread2");
+
+//    autoResetEvent.Set();
+//});
+
+//Thread thread3 = new(() =>
+//{
+//    //Thread1 işlemi bittikten sonra thread2 işlemi başlar. thread2 şuan beklemesini sağlıyoruz.
+//    autoResetEvent.WaitOne();
+//    Console.WriteLine("Thread3");
+
+
+//    autoResetEvent.Set();
+//});
+
+//thread1.Start();
+//thread2.Start();
+//thread3.Start();
+//    }
+//}
+#endregion
+
+#region ManualResetEventSlim
 internal class Program
 {
     private static void Main(string[] args)
     {
-        //Örneğin thread1 işlemini bitirdikten sonra thread2 işlemi başlamasını istiyorsak EventWaitHandle kullanabiliriz.
-        //AutoReset her zaman otomatik olarak resetlenir bunun faydası ise bir thread işlemi bittiğinde diğer thread işlemi başlar.
-        //Her bir thread kuyruğa alınır ve sırayla işlem yaparlar.
-        AutoResetEvent autoResetEvent = new(false);
+        //ManualResetEventSlim ile AutoResetEvent arasındaki fark ise ManualResetEventSlim ile birden fazla thread işlemi başlatılabilir.
+        ManualResetEventSlim manualResetEventSlim = new(false);
 
         Thread thread1 = new(() =>
         {
             Console.WriteLine("Thread1");
 
-            //Thread1 işlemi bittiğinde autoResetEvent.Set() metodu ile thread2 işlemine geçiş yapılmasını sağlıyoruz.
-            //Bekleyen threadlerden sadece bir tanesi başlatılır.
-            autoResetEvent.Set();
+            //Set komutu ile diğer threadlerin işlem yapmasını sağlıyoruz.
+            manualResetEventSlim.Set();
+
+       
         });
 
         Thread thread2 = new(() =>
         {
-            //Thread1 işlemi bittikten sonra thread2 işlemi başlar. thread2 şuan beklemesini sağlıyoruz.
-            autoResetEvent.WaitOne();
-            Console.WriteLine("Thread2");
 
-            autoResetEvent.Set();
+            for (int i = 0; i < 5; i++)
+            {
+                manualResetEventSlim.Wait();
+
+                Console.WriteLine("Thread2");
+
+                //Reset komutu ile diğer threadlerin işlem yapmasını engelliyoruz. Diğer threadler işlem yapamaz.
+                manualResetEventSlim.Reset();
+            }
         });
 
         Thread thread3 = new(() =>
         {
-            //Thread1 işlemi bittikten sonra thread2 işlemi başlar. thread2 şuan beklemesini sağlıyoruz.
-            autoResetEvent.WaitOne();
-            Console.WriteLine("Thread3");
+            manualResetEventSlim.Wait();
+            for (int i = 0; i < 5; i++)
+            {
+                manualResetEventSlim.Wait();
+                Console.WriteLine("Thread3");
 
+                manualResetEventSlim.Reset();
 
-            autoResetEvent.Set();
+            }
         });
 
         thread1.Start();
@@ -42,5 +94,4 @@ internal class Program
         thread3.Start();
     }
 }
-
 #endregion
