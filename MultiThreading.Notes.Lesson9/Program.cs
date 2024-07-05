cf2d97f3affaa15e5e4f7a737048559503e3e1f7
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-07-05 08:58:10 +0300

    EventWaitHandle ile ilgili örnek gerçekleştirildi

--- MultiThreading.Notes.Lesson9/Program.cs
+++ MultiThreading.Notes.Lesson9/Program.cs
@@ -45,48 +45,88 @@
 #endregion
 
 #region ManualResetEventSlim
+//internal class Program
+//{
+//    private static void Main(string[] args)
+//    {
+//        //ManualResetEventSlim ile AutoResetEvent arasındaki fark ise ManualResetEventSlim ile birden fazla thread işlemi başlatılabilir.
+//        ManualResetEventSlim manualResetEventSlim = new(false);
+
+//        Thread thread1 = new(() =>
+//        {
+//            Console.WriteLine("Thread1");
+
+//            //Set komutu ile diğer threadlerin işlem yapmasını sağlıyoruz.
+//            manualResetEventSlim.Set();
+
+
+//        });
+
+//        Thread thread2 = new(() =>
+//        {
+
+//            for (int i = 0; i < 5; i++)
+//            {
+//                manualResetEventSlim.Wait();
+
+//                Console.WriteLine("Thread2");
+
+//                //Reset komutu ile diğer threadlerin işlem yapmasını engelliyoruz. Diğer threadler işlem yapamaz.
+//                manualResetEventSlim.Reset();
+//            }
+//        });
+
+//        Thread thread3 = new(() =>
+//        {
+//            for (int i = 0; i < 5; i++)
+//            {
+//                manualResetEventSlim.Wait();
+//                Console.WriteLine("Thread3");
+
+//                manualResetEventSlim.Reset();
+
+//            }
+//        });
+
+//        thread1.Start();
+//        thread2.Start();
+//        thread3.Start();
+//    }
+//}
+#endregion
+
+#region EventWaitHandle
 internal class Program
 {
     private static void Main(string[] args)
     {
-        //ManualResetEventSlim ile AutoResetEvent arasındaki fark ise ManualResetEventSlim ile birden fazla thread işlemi başlatılabilir.
-        ManualResetEventSlim manualResetEventSlim = new(false);
+
+        //EventWaitHandle, AutoResetEvent ve ManuelEvenetResetSlim'in birleşimini düşünebiliriz. Ortak bir isimlendirme yapmasını sağlar. Örneğin AutoResetEvent da WaitOne ve Set metotları vardır. ManuelResetEventSlim de Wait ve Set metotları vardır. EventWaitHandle bu metotları ortak hale getirir.
+
+        //EventWaitHandle eventWaitHandle = new(false, EventResetMode.AutoReset);
+        EventWaitHandle eventWaitHandle = new(false, EventResetMode.ManualReset);
 
         Thread thread1 = new(() =>
         {
             Console.WriteLine("Thread1");
 
             //Set komutu ile diğer threadlerin işlem yapmasını sağlıyoruz.
-            manualResetEventSlim.Set();
+            eventWaitHandle.Set();
 
-       
+
         });
 
         Thread thread2 = new(() =>
         {
-
-            for (int i = 0; i < 5; i++)
-            {
-                manualResetEventSlim.Wait();
-
-                Console.WriteLine("Thread2");
-
-                //Reset komutu ile diğer threadlerin işlem yapmasını engelliyoruz. Diğer threadler işlem yapamaz.
-                manualResetEventSlim.Reset();
-            }
+            eventWaitHandle.WaitOne();
+            Console.WriteLine("Thread2");
         });
 
         Thread thread3 = new(() =>
         {
-            manualResetEventSlim.Wait();
-            for (int i = 0; i < 5; i++)
-            {
-                manualResetEventSlim.Wait();
-                Console.WriteLine("Thread3");
+            eventWaitHandle.WaitOne();
+            Console.WriteLine("Thread3");
 
-                manualResetEventSlim.Reset();
-
-            }
         });
 
         thread1.Start();
