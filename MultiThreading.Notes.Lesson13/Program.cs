199ae7e44a83c88bee392a61946dcd69c48cf3a1
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-07-08 12:05:28 +0300

    System.Threading.Timer ile ilgili örnek gerçekleştirildi

---
+++ MultiThreading.Notes.Lesson13/Program.cs
@@ -0,0 +1,13 @@
+﻿#region System.Threading.Timer
+Timer timer = new(state =>
+{
+Console.WriteLine(state);
+}, "Tick!", 15000, 100); //15 saniye sonra 100 ms'de bir console writeline çalıştırıyor. 100 yerine Timeout.Infinite diyerek sonsuz döngü yapılabilir.
+
+Thread.Sleep(20000);
+
+//Periyodik çalışma zamanı değiştirmek için kullanılır
+timer.Change(0, 1500); //artık 1.5 saniyede bir çalışacak.
+
+Console.ReadLine(); 
+#endregion
