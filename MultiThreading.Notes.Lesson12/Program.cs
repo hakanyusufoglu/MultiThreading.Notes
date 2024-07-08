b213ea7d8cb6ae8ba6919309df9035cccc11b43c
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-07-08 11:30:31 +0300

    Thread local storage kapsamında ThreadStatic ile ilgili örnek gerçekleştirildi

---
+++ MultiThreading.Notes.Lesson12/Program.cs
@@ -0,0 +1,34 @@
+﻿#region ThreadStatic Attribute
+
+//Thread local storage (TLS) bir threadin kendi verilerini saklamasını sağlar. Ayrıca bu veriler diğer threadler tarafından değiştirilemez.
+public class Program
+{
+    //ThreadStatic attribute ile bir field'ın thread local storage olarak işaretlenmesini sağlar. 
+    [ThreadStatic]
+    static int x = 0;
+    private static void Main(string[] args)
+    {
+        Thread thread1 = new(() =>
+        {
+            while (x < 10)
+                Console.WriteLine($"Thread1 {++x}");
+        });
+
+        Thread thread2 = new(() =>
+        {
+            while (x < 10)
+                Console.WriteLine($"Thread2 {++x}");
+        });
+
+        Thread thread3 = new(() =>
+        {
+            while (x < 10)
+                Console.WriteLine($"Thread3 {++x}");
+        });
+
+        thread1.Start();
+        thread2.Start();
+        thread3.Start();
+    }
+}
+#endregion
