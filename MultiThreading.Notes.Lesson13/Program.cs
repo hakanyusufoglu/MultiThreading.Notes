d07d3edb0ad65837061437825c552ce09b1cdb84
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-07-08 12:12:31 +0300

    System.Timers.Timer ile ilgili örnek gerçekleştirildi

--- MultiThreading.Notes.Lesson13/Program.cs
+++ MultiThreading.Notes.Lesson13/Program.cs
@@ -1,13 +1,28 @@
 ﻿#region System.Threading.Timer
-Timer timer = new(state =>
+//Timer timer = new(state =>
+//{
+//Console.WriteLine(state);
+//}, "Tick!", 15000, 100); //15 saniye sonra 100 ms'de bir console writeline çalıştırıyor. 100 yerine Timeout.Infinite diyerek sonsuz döngü yapılabilir.
+
+//Thread.Sleep(20000);
+
+////Periyodik çalışma zamanı değiştirmek için kullanılır
+//timer.Change(0, 1500); //artık 1.5 saniyede bir çalışacak.
+
+//Console.ReadLine(); 
+#endregion
+
+System.Timers.Timer timer = new();
+timer.Elapsed += (sender, e) =>
 {
-Console.WriteLine(state);
-}, "Tick!", 15000, 100); //15 saniye sonra 100 ms'de bir console writeline çalıştırıyor. 100 yerine Timeout.Infinite diyerek sonsuz döngü yapılabilir.
+    Console.WriteLine("Tick !");
+};
+timer.Start();
+timer.Interval = 500;
+timer.Stop();
 
-Thread.Sleep(20000);
+Thread.Sleep(2000);
+Console.Read();
+#region System.Timers.Timer
 
-//Periyodik çalışma zamanı değiştirmek için kullanılır
-timer.Change(0, 1500); //artık 1.5 saniyede bir çalışacak.
-
-Console.ReadLine(); 
 #endregion
