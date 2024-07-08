f89b8d9482d6983204e810b52e463bf1ab4d652f
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-07-08 14:08:20 +0300

    Task.Run ile ilgili örnek gerçekleştirildi

--- MultiThreading.Notes.Lesson14/Program.cs
+++ MultiThreading.Notes.Lesson14/Program.cs
@@ -1,10 +1,20 @@
 ﻿#region New Task
-Task task = new Task(() =>
+//Task task = new Task(() =>
+//{
+//    for (int i = 0; i < 10; i++)
+//        Console.WriteLine(i);
+//});
+//task.Start();
+
+//Console.Read();
+#endregion
+
+#region Task.Run
+Task task = Task.Run(() =>
 {
     for (int i = 0; i < 10; i++)
         Console.WriteLine(i);
 });
-task.Start();
 
 Console.Read();
 #endregion
