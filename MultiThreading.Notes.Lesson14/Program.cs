aaba77a19ab9789eb0fef44ec32df4da7487fb54
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-07-08 14:12:10 +0300

    Task.Factory.StartNew ile ilgili özellik gerçekleştirildi

--- MultiThreading.Notes.Lesson14/Program.cs
+++ MultiThreading.Notes.Lesson14/Program.cs
@@ -10,7 +10,20 @@
 #endregion
 
 #region Task.Run
-Task task = Task.Run(() =>
+//Task task = Task.Run(() =>
+//{
+//    for (int i = 0; i < 10; i++)
+//        Console.WriteLine(i);
+//});
+
+//Console.Read();
+#endregion
+
+#region Task.Factory.StartNew
+//Task.Factory.StartNew ile birlikte task içerisinde task çalıştırabiliriz. Ancak diğer new Task() ve Task.Run() metodların da çalıştıramayız.
+//StartNew de daha fazla parametre alabiliriz. Örneğin TaskCreationOptions, TaskScheduler gibi.
+//Genel de Task.Run() kullanılır.
+Task task = Task.Factory.StartNew(() =>
 {
     for (int i = 0; i < 10; i++)
         Console.WriteLine(i);
