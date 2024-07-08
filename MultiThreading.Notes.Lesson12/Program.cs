054f3babd27ad9c047243d7ccc2baccd97d6c5ae
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-07-08 11:35:00 +0300

    ThreadLocal<T> sınıfı ile ilgili örnek gerçekleştirildi

--- MultiThreading.Notes.Lesson12/Program.cs
+++ MultiThreading.Notes.Lesson12/Program.cs
@@ -1,34 +1,63 @@
 ﻿#region ThreadStatic Attribute
 
 //Thread local storage (TLS) bir threadin kendi verilerini saklamasını sağlar. Ayrıca bu veriler diğer threadler tarafından değiştirilemez.
-public class Program
+//public class Program
+//{
+//    //ThreadStatic attribute ile bir field'ın thread local storage olarak işaretlenmesini sağlar. 
+//    [ThreadStatic]
+//    static int x = 0;
+//    private static void Main(string[] args)
+//    {
+//        Thread thread1 = new(() =>
+//        {
+//            while (x < 10)
+//                Console.WriteLine($"Thread1 {++x}");
+//        });
+
+//        Thread thread2 = new(() =>
+//        {
+//            while (x < 10)
+//                Console.WriteLine($"Thread2 {++x}");
+//        });
+
+//        Thread thread3 = new(() =>
+//        {
+//            while (x < 10)
+//                Console.WriteLine($"Thread3 {++x}");
+//        });
+
+//        thread1.Start();
+//        thread2.Start();
+//        thread3.Start();
+//    }
+//}
+#endregion
+
+#region ThreadLocal<T> Class
+//Thread local storage (TLS) bir threadin kendi verilerini saklamasını sağlar. Ayrıca bu veriler diğer threadler tarafından değiştirilemez.
+//ThreadLocal<T> sınıfı, bir threadin kendi verilerini saklamasını sağlar. Ayrıca bu veriler diğer threadler tarafından değiştirilemez. Hem static hem de instance field olarak tanımlanabilir. Örneğin X değişkeni statik değilse bile ThreadLocal<T> sınıfı ile thread local storage olarak işaretlenebilir.
+
+//0 başlangıç değeridir.
+ThreadLocal<int> x = new(() => 0);
+Thread thread1 = new(() =>
 {
-    //ThreadStatic attribute ile bir field'ın thread local storage olarak işaretlenmesini sağlar. 
-    [ThreadStatic]
-    static int x = 0;
-    private static void Main(string[] args)
-    {
-        Thread thread1 = new(() =>
-        {
-            while (x < 10)
-                Console.WriteLine($"Thread1 {++x}");
-        });
+    while (x.Value < 10)
+        Console.WriteLine($"Thread1 {++x.Value}");
+});
 
-        Thread thread2 = new(() =>
-        {
-            while (x < 10)
-                Console.WriteLine($"Thread2 {++x}");
-        });
+Thread thread2 = new(() =>
+{
+    while (x.Value < 10)
+        Console.WriteLine($"Thread2 {++x.Value}");
+});
 
-        Thread thread3 = new(() =>
-        {
-            while (x < 10)
-                Console.WriteLine($"Thread3 {++x}");
-        });
+Thread thread3 = new(() =>
+{
+    while (x.Value < 10)
+        Console.WriteLine($"Thread3 {++x.Value}");
+});
 
-        thread1.Start();
-        thread2.Start();
-        thread3.Start();
-    }
-}
+thread1.Start();
+thread2.Start();
+thread3.Start();
 #endregion
