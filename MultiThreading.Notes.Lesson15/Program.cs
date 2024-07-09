16b9d903ce7e69d87bef040c2ba78371d3f97bad
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-07-09 08:53:42 +0300

    ContinueWhenAny ile ilgili örnek gerçekleştirildi

--- MultiThreading.Notes.Lesson15/Program.cs
+++ MultiThreading.Notes.Lesson15/Program.cs
@@ -14,6 +14,33 @@
 #region ContinueWhenAll
 //ContinueWhenAll tüm threadler görevlerini tamamladıktan sonra çalışır. ve bu işlemi bir thread üzerinde yapar. Böylece asenkrondur.
 
+//Task task1 = Task.Run(() =>
+//{
+//    for (int i = 0; i < 10; i++)
+//        Console.WriteLine($"Task 1: {i}");
+//});
+
+//Task task2 = Task.Run(() =>
+//{
+//    for (int i = 0; i < 10; i++)
+//        Console.WriteLine($"Task 2: {i}");
+//});
+
+//Task task3 = Task.Run(() =>
+//{
+//    for (int i = 0; i < 10; i++)
+//        Console.WriteLine($"Task 3: {i}");
+//});
+
+//TaskFactory taskFactory = new();
+//taskFactory.ContinueWhenAll(new [] { task1, task2, task3 }, tasks =>
+//{
+//    Console.WriteLine("All tasks are completed");
+//});
+#endregion
+
+#region ContinueWhenAny
+//ContinueWhenAny herhangi bir thread görevini tamamladıktan sonra çalışır. ve bu işlemi bir thread üzerinde yapar. Böylece asenkrondur.
 Task task1 = Task.Run(() =>
 {
     for (int i = 0; i < 10; i++)
@@ -33,9 +60,9 @@ Task task3 = Task.Run(() =>
 });
 
 TaskFactory taskFactory = new();
-taskFactory.ContinueWhenAll(new [] { task1, task2, task3 }, tasks =>
+taskFactory.ContinueWhenAny(new[] { task1, task2, task3 }, tasks =>
 {
-    Console.WriteLine("All tasks are completed");
+    Console.WriteLine("...");
 });
 #endregion
 #endregion
