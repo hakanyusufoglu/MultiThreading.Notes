86e7d324a9559005efaaf942e7e490c135f02f44
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-07-10 15:09:21 +0300

    ConcurrentStack<T> ile ilgili örnek gerçekleştirildi

--- MultiThreading.Notes.Lesson19/Program.cs
+++ MultiThreading.Notes.Lesson19/Program.cs
@@ -40,47 +40,83 @@
 #endregion
 
 #region BlockingCollection<T>
-// BlockingCollection<T> sınıfı şu işe yarar; bir thread bir işlemi yaparken diğer thread bir işlem yapamaz. BlockingCollection<T> sınıfı bu işlemi yaparken diğer thread'lerin işlem yapmasını engeller. BlockingCollection<T> sınıfı içerisindeki elemanlar üzerinde işlem yapmak için kullanılır.
+//// BlockingCollection<T> sınıfı şu işe yarar; bir thread bir işlemi yaparken diğer thread bir işlem yapamaz. BlockingCollection<T> sınıfı bu işlemi yaparken diğer thread'lerin işlem yapmasını engeller. BlockingCollection<T> sınıfı içerisindeki elemanlar üzerinde işlem yapmak için kullanılır.
 
-// BlockingCollection<T> sınıfı içerisindeki elemanlar üzerinde işlem yapmak için Take() metodu kullanılır. Take() metodu eleman çıkartır ve eleman çıkartıldıktan sonra elemanın çıkartılıp çıkartılmadığını kontrol eder. Eğer eleman çıkartıldıysa işlem yapar, çıkartılmadıysa işlem yapmaz.
+//// BlockingCollection<T> sınıfı içerisindeki elemanlar üzerinde işlem yapmak için Take() metodu kullanılır. Take() metodu eleman çıkartır ve eleman çıkartıldıktan sonra elemanın çıkartılıp çıkartılmadığını kontrol eder. Eğer eleman çıkartıldıysa işlem yapar, çıkartılmadıysa işlem yapmaz.
 
-BlockingCollection<int> numbers = new();
+//BlockingCollection<int> numbers = new();
 
-Task producer = Task.Run(async () =>
-{
-    for (int i = 0; i < 10; i++)
-    {
-        numbers.Add(i);
-        await Console.Out.WriteLineAsync($"Producer {i.ToString()}");
-        await Task.Delay(500);
-    }
+//Task producer = Task.Run(async () =>
+//{
+//    for (int i = 0; i < 10; i++)
+//    {
+//        numbers.Add(i);
+//        await Console.Out.WriteLineAsync($"Producer {i.ToString()}");
+//        await Task.Delay(500);
+//    }
 
-    //Ekleme işlemi son bulduğunu belirtmek için CompleteAdding() metodu kullanılır.
+//    //Ekleme işlemi son bulduğunu belirtmek için CompleteAdding() metodu kullanılır.
 
-    //Ne zaman bu metot çağrılırsa consumer tetiklenir ve işlem yapmaya başlar. (Gerçek queue publisher ve consumer mantığını düşünebiliriz)
-    //Tüm veriler tüketilirse hata fırlatmasını sağlar
-    //CompleteAdding() metodu kullanılmadığında consumer işlem yapmaya devam eder ancak kaynak tüketildiğini anlayamayacağı için hata fırlatmaz. 
-    numbers.CompleteAdding();
-});
+//    //Ne zaman bu metot çağrılırsa consumer tetiklenir ve işlem yapmaya başlar. (Gerçek queue publisher ve consumer mantığını düşünebiliriz)
+//    //Tüm veriler tüketilirse hata fırlatmasını sağlar
+//    //CompleteAdding() metodu kullanılmadığında consumer işlem yapmaya devam eder ancak kaynak tüketildiğini anlayamayacağı için hata fırlatmaz. 
+//    numbers.CompleteAdding();
+//});
 
-Task consumer = Task.Run(async () =>
-{
-    try
-    {
-        while (true)
-        {
-            //BlockingCollection<T> TryTake () metodu ile eleman çıkartılır. Eğer eleman yoksa işlem yapmaya devam eder.
-            int result = numbers.Take();
-            await Console.Out.WriteLineAsync($"Consumer {result}");
-        }
-    }
-    catch (Exception)
-    {
-        await Console.Out.WriteLineAsync("Kaynak tüketildi...");
-    }
-});
+//Task consumer = Task.Run(async () =>
+//{
+//    try
+//    {
+//        while (true)
+//        {
+//            //BlockingCollection<T> TryTake () metodu ile eleman çıkartılır. Eğer eleman yoksa işlem yapmaya devam eder.
+//            int result = numbers.Take();
+//            await Console.Out.WriteLineAsync($"Consumer {result}");
+//        }
+//    }
+//    catch (Exception)
+//    {
+//        await Console.Out.WriteLineAsync("Kaynak tüketildi...");
+//    }
+//});
 
-await Task.WhenAny(producer, consumer);
+//await Task.WhenAny(producer, consumer);
 
-Console.ReadLine();
+//Console.ReadLine();
 #endregion
+
+#region ConcurrentStack<T>
+////ConcurrentStack<T> sınıfı thread güvenli bir şekilde eleman eklemek ve çıkarmak için LIFO (last in first out)mantığı ile haraket eder.
+
+//ConcurrentStack<int> numbers = new();
+
+//Task producer = Task.Run(async () =>
+//{
+//    for (int i = 0; i < 10; i++)
+//    {
+//        numbers.Push(i);
+//        Console.WriteLine($"Producer {i}");
+//        await Task.Delay(100);
+//    }
+//});
+
+//Task consumer = Task.Run(async () =>
+//{
+//    await Task.Delay(3000);
+
+//    while (true)
+//    {
+//        if (numbers.TryPop(out int result))
+//        {
+//            Console.WriteLine($"Consumer {result}");
+//            await Task.Delay(100);
+
+//        }
+//    }
+//});
+
+//await Task.WhenAny(producer, consumer);
+
+//Console.ReadLine();
+#endregion
+
