61bbdbcc18acab8eef0ee685cc7201ff5662c779
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-07-10 09:07:33 +0300

    ConfigureAwait ile ilgili örnek gerçekleştirildi

---
+++ MultiThreading.Notes.Lesson17/Program.cs
@@ -0,0 +1,27 @@
+﻿#region ConfigureAwait
+// ConfigureAwait(false) şu işe yarar. Eğer bir task'in sonucunu kullanmayacaksak ve sadece bir işlemi başlatıp sonucunu kullanmayacaksak, 
+// bu durumda ConfigureAwait(false) kullanarak işlemi başka bir thread üzerinde çalıştırabiliriz. 
+// Bu sayede işlemi başka bir thread üzerinde çalıştırarak mevcut thread üzerindeki işlemlerin bloklanmasını engelleyebiliriz. 
+// Bu sayede performansı arttırabiliriz.
+async Task<string> ReadFileAsync(string path)
+{
+    StreamReader reader = new StreamReader(path);
+    // Buradaki işlemden sonra bir task döndürüyoruz. Bu task'in sonucunu kullanacağız, bu durumda ConfigureAwait(false) kullanarak 
+    // işlemin bitişini başka bir thread üzerinde bekleyebiliriz. Ancak dönen sonucu burada kullanmak istediğimiz için dikkatli olmalıyız.
+
+    // ConfigureAwait(true) kullanarak işlemi mevcut thread üzerinde çalıştırabiliriz. ve işlemler mevcut thread üzerinden devam eder.
+    var content = await reader.ReadToEndAsync().ConfigureAwait(false);
+
+    // ConfigureAwait(false) kullanarak işlemin devamının başka bir thread üzerinde çalışmasına izin veriyoruz. 
+    // Ancak bu durumda UI thread'i gerekmeyen bir console uygulaması olduğundan, devam eden işlemler thread bağımsız olabilir.
+    Console.WriteLine($"End.");
+
+    return content;
+}
+
+var filePath = Path.Combine(AppContext.BaseDirectory, "textfile.txt");
+var content = await ReadFileAsync(filePath);
+
+Console.WriteLine($"Content: {content}");
+
+#endregion
